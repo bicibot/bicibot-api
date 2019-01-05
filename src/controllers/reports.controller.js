e090e6f69795cfca39b0b1e61cbb6f1c334bd9f0
Pedro Paes <iacapuca@gmail.com>
2019-01-05 20:59:40 -0300

    Added Geocoder to reportController

--- src/controllers/reports.controller.js
+++ src/controllers/reports.controller.js
@@ -1,5 +1,10 @@
 import Report from "../models/reports.model";
+import geo from "mapbox-geocoding";
+const { WebhookClient, Card, Suggestion } = require("dialogflow-fulfillment");
+
 require("dotenv").config();
+
+geo.setAccessToken(process.env.mapboxToken);
 
 const controller = {};
 
@@ -18,15 +23,56 @@ controller.getAll = async (req, res) => {
 
 controller.addReport = async (req, res) => {
   if (req.header("authToken") === process.env.authToken) {
-    let reportToAdd = new Report({
-      description: req.body.description
-    });
     try {
-      const savedReport = await Report.addReport(reportToAdd);
-      res.send(savedReport);
+      const agent =  new WebhookClient({ request: req, response: res });
+
+      await function addReport(agent) {
+        // let reportToAdd = new Report({
+        //   description: req.body.description
+        // });
+        // try {
+        //   const savedReport = await Report.addReport(reportToAdd);
+        //   res.send(savedReport);
+        // } catch (err) {
+        //   res.status(400);
+        //   res.send(err);
+        // }
+        // } else {
+        //   console.log("Entrou no Else");
+        //   res.status(401);
+        //   res.send();
+        // }
+
+        // let address = geo.geocode('mapbox.places', agent.parameters.location["street-address"], function (err, geoData) {
+        //   return new Promise (function(resolve, reject) {
+        //     if (geoData) {
+        //       resolve(geoData.features[0].center);
+        //     } else {
+        //       reject(new Error(err));
+        //     }
+        //   })
+        // });
+
+        // geoCoding(agent.parameters.location["street-address"]).then(a => {
+        //   console.log(a);
+        // })
+
+        const geoCoding = async address => {
+          await geo.geocode("mapbox.places", address, function(err, geoData) {
+            return geoData.features[0].center;
+          });
+        };
+
+        const location = geoCoding(agent.parameters.location["street-address"]);
+        console.log(location);
+
+        return agent.add(`Sua localização é ${location}`);
+      };
+      let intentMap = new Map();
+      intentMap.set("denuncia.tipo - no - address", addReport);
+      agent.handleRequest(intentMap);
     } catch (err) {
-      res.status(400);
-      res.send(err);
+      console.log(err);
     }
   } else {
     res.status(401);

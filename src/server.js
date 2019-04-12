93c4b9ff0792cdaff22323cdf3ec8f8b94835379
Pedro Paes <iacapuca@gmail.com>
2019-04-11 21:20:42 -0300

    Added subdomain option

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import router from './routes/index';
 import morgan from "morgan";
 import connectToDb  from "./config/db";
+import subdomain from "express-subdomain";
 require("dotenv").config();
 
 connectToDb();
@@ -11,13 +12,13 @@ connectToDb();
 const app = express();
 const PORT = process.env.port || 3000;
 
+app.use(subdomain('api', router));
+
 app.use(morgan("dev"));
 app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);

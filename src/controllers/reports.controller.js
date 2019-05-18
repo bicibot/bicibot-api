2bc480c591315daf3230bdd3434f0539ab1c5500
Pedro Paes <iacapuca@gmail.com>
2019-05-18 13:18:06 -0300

    Added Plate to report

--- src/controllers/reports.controller.js
+++ src/controllers/reports.controller.js
@@ -1,9 +1,9 @@
-import geo from 'mapbox-geocoding';
-import Report from '../models/reports.model';
+import geo from "mapbox-geocoding";
+import Report from "../models/reports.model";
 
-const { WebhookClient } = require('dialogflow-fulfillment');
+const { WebhookClient } = require("dialogflow-fulfillment");
 
-require('dotenv').config();
+require("dotenv").config();
 
 geo.setAccessToken(process.env.mapboxToken);
 
@@ -23,50 +23,58 @@ controller.getAll = async (req, res) => {
 };
 
 controller.addReport = async (req, res) => {
-  if (req.header('authToken') === process.env.authToken) {
+  if (req.header("authToken") === process.env.authToken) {
     try {
       const agent = new WebhookClient({ request: req, response: res });
-
-      const addReport = async function addReport() {
-        // console.log(agent.contexts[0].parameters); -> A way to access parameter of other contexts
+      const addReport = async () => {
+        // TODO: Function refactor
         function geoCoding(address) {
-          return new Promise(function (resolve, reject) {
-            geo.geocode('mapbox.places', address, function (err, res) {
-              console.log(res);
+          return new Promise(function(resolve, reject) {
+            geo.geocode("mapbox.places", address, function(err, res) {
               if (!err) {
                 resolve(res.features[0].center);
               } else {
-                reject(new Error('Endereço não encontrado'));
+                reject(new Error("Endereço não encontrado"));
               }
             });
           });
         }
 
-        const latlng = await geoCoding(agent.contexts[0].parameters.location['street-address']);
+        const isPlateValid = async plate => {
+          let plateRegex = /^[a-zA-Z]{3}[0-9]{4}\b/; // Mover para arquivo de constantes
+          return plateRegex.test(plate.replace("-", ""));
+        };
+
+        const latlng = await geoCoding(
+          agent.contexts[0].parameters.location["street-address"]
+        );
+        const plate = (await isPlateValid(agent.parameters["report-plate"]))
+          ? agent.parameters["report-plate"]
+          : new Error("Placa invalida");
+
         const reportToAdd = new Report({
-          description: agent.contexts[0].parameters['report-description'],
-          report_type: agent.contexts[0].parameters['report-type'],
+          description: agent.contexts[0].parameters["report-description"],
+          report_type: agent.contexts[0].parameters["report-type"],
           location: latlng,
+          plate: plate
         });
 
         try {
           await Report.addReport(reportToAdd);
           res.status(201);
-          return agent.add('Denuncia realizada com sucesso.');
+          return agent.add("Denuncia realizada com sucesso.");
         } catch (err) {
-          console.log(err);
           res.status(400);
           res.send(err);
-          agent.add('Ocorreu um erro, por favor, tente novamente.');
+          agent.add("Ocorreu um erro, por favor, tente novamente.");
         }
       };
 
       const intentMap = new Map();
-      intentMap.set('denuncia2', addReport);
-      intentMap.set('denuncia2 - yes', addReport);
+      intentMap.set("denuncia2", addReport);
+      intentMap.set("denuncia2 - yes", addReport);
       agent.handleRequest(intentMap);
     } catch (err) {
-      console.log(err);
       res.status(400);
       res.send(err);
     }

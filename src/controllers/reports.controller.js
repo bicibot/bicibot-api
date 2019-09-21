4dfd311918675cbfedde46dbb0af809e4a53fdd6
Pedro Luiz <iacapuca@gmail.com>
2019-09-21 11:23:12 -0300

    Added Twitter API POST

--- src/controllers/reports.controller.js
+++ src/controllers/reports.controller.js
@@ -1,9 +1,17 @@
 import geo from "mapbox-geocoding";
 import Report from "../models/reports.model";
+import Twitter from "twitter";
 
 require("dotenv").config();
 
 geo.setAccessToken(process.env.mapboxToken);
+
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
 const controller = {};
 
@@ -30,9 +38,22 @@ controller.addReport = async (req, res) => {
     return obj;
   };
 
+  const tweet = async report => {
+    client
+      .post("statuses/update", { status: report.description })
+      .then(function(tweet) {
+        console.log(tweet);
+      })
+      .catch(function(error) {
+        throw error;
+      });
+  };
+
   if (req.header("authToken") === process.env.authToken) {
     try {
-      await Report.addReport(removeEmpty(req.body));
+      let report = removeEmpty(req.body);
+      await Report.addReport(report);
+      await tweet(report);
       res.status(201);
       res.send("Denuncia realizada com sucesso");
     } catch (err) {
